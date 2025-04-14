1d06c2bc74d5b6620552ca0841b912565c58c8f7
yxu <penguinxx2@gmail.com>
2025-04-14 23:08:01 +0900

    add nodes

--- src/maps/centerMarshForest.ts
+++ src/maps/centerMarshForest.ts
@@ -3,13 +3,90 @@ import { NodeType, CampNode, NormalMonsterNode, EmptyNode } from "./Node";
 import { Region } from "./Region";
 import centerMarshForestImage from "../assets/centerMarshForest.png";
 
-export const centerMarshForestToEastNode: EmptyNode = {
+export const centerMarshForestCanyonEntranceNode: NormalMonsterNode = {
+  name: "溪谷入口",
+  id: "centerMarshForestCanyonEntrance",
+  description: "溪谷入口",
+  type: NodeType.NormalMonster,
+  position: { x: 890, y: 390 },
+  toNodeList: [],
+  monsterList: [
+    {
+      monster: CreatureType.Slime,
+      maxLevel: 1,
+      minLevel: 1,
+      weight: 1,
+    },
+  ],
+};
+
+export const centerMarshForestToCanyonNode: NormalMonsterNode = {
+  name: "前往溪谷的路",
+  id: "centerMarshForestToCanyon",
+  description: "前往溪谷的路",
+  type: NodeType.NormalMonster,
+  position: { x: 950, y: 430 },
+  toNodeList: [centerMarshForestCanyonEntranceNode],
+  monsterList: [
+    {
+      monster: CreatureType.Slime,
+      maxLevel: 1,
+      minLevel: 1,
+      weight: 1,
+    },
+  ],
+};
+
+export const centerMarshForestHillFootNode: NormalMonsterNode = {
+  name: "山脚下",
+  id: "centerMarshForestHillFoot",
+  description: "山脚下",
+  type: NodeType.NormalMonster,
+  position: { x: 640, y: 370 },
+  toNodeList: [],
+  monsterList: [
+    {
+      monster: CreatureType.Slime,
+      maxLevel: 1,
+      minLevel: 1,
+      weight: 1,
+    },
+  ],
+};
+
+export const centerMarshForestFarRoadNode: NormalMonsterNode = {
+  name: "路的远方",
+  id: "centerMarshForestFarRoad",
+  description: "路的远方",
+  type: NodeType.NormalMonster,
+  position: { x: 790, y: 440 },
+  toNodeList: [centerMarshForestHillFootNode, centerMarshForestToCanyonNode],
+  monsterList: [
+    {
+      monster: CreatureType.Slime,
+      maxLevel: 1,
+      minLevel: 1,
+      weight: 1,
+    },
+  ],
+};
+
+export const centerMarshForestToVeryEastNode: EmptyNode = {
   name: "一路向东",
-  id: "centerMarshForestToEast",
+  id: "centerMarshForestToVeryEast",
   description: "一路向东",
   type: NodeType.Empty,
-  position: { x: 1150, y: 500 },
+  position: { x: 1500, y: 450 },
   toNodeList: [],
+};
+
+export const centerMarshForestToEastNode: EmptyNode = {
+  name: "向东行",
+  id: "centerMarshForestToEast",
+  description: "向东行",
+  type: NodeType.Empty,
+  position: { x: 1150, y: 500 },
+  toNodeList: [centerMarshForestToVeryEastNode],
 };
 
 export const centerMarshForestOppositeRoadNode: EmptyNode = {
@@ -21,13 +98,13 @@ export const centerMarshForestOppositeRoadNode: EmptyNode = {
   toNodeList: [centerMarshForestToEastNode],
 };
 
-export const centerMarshForestFarRoadNode: NormalMonsterNode = {
-  name: "远方的路",
-  id: "centerMarshForestFarRoad",
-  description: "远方的路",
+export const centerMarshForestRoadNode: NormalMonsterNode = {
+  name: "顺路",
+  id: "centerMarshForestRoad",
+  description: "顺路",
   type: NodeType.NormalMonster,
   position: { x: 450, y: 450 },
-  toNodeList: [],
+  toNodeList: [centerMarshForestFarRoadNode],
   monsterList: [
     {
       monster: CreatureType.Slime,
@@ -61,7 +138,7 @@ export const centerMarshForestNearRuinNode: CampNode = {
   description: "遗迹周边",
   type: NodeType.Camp,
   position: { x: 170, y: 900 },
-  toNodeList: [centerMarshForestRiverBankNode, centerMarshForestFarRoadNode],
+  toNodeList: [centerMarshForestRiverBankNode, centerMarshForestRoadNode],
 };
 
 export const centerMarshForestRegion: Region = {
@@ -70,7 +147,7 @@ export const centerMarshForestRegion: Region = {
   description: "池沼森林中部 黄金平原",
   isOpen: true,
   startNode: centerMarshForestNearRuinNode,
-  nodeList: [centerMarshForestNearRuinNode, centerMarshForestRiverBankNode, centerMarshForestFarRoadNode, centerMarshForestOppositeRoadNode, centerMarshForestToEastNode],
+  nodeList: [centerMarshForestNearRuinNode, centerMarshForestRiverBankNode, centerMarshForestRoadNode, centerMarshForestFarRoadNode, centerMarshForestHillFootNode, centerMarshForestOppositeRoadNode, centerMarshForestToEastNode, centerMarshForestToVeryEastNode],
   mapImage: centerMarshForestImage,
   mapWidth: 1536,
   mapHeight: 1024,
